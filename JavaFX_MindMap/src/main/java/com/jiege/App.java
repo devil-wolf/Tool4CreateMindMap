28eecdba0ffb2d2d88feec96b847ba8d0ac09bf5
Alilestera <alilestera1404@outlook.com>
2022-03-20 20:37:47 +0800

    initial Application run method

---
+++ JavaFX_MindMap/src/main/java/com/jiege/App.java
@@ -0,0 +1,15 @@
+package com.jiege;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Alilestera
+ * @date 3/20/2022
+ */
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
